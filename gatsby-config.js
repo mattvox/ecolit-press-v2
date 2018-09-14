5680cfcf1cbd22e57339dea0872c6a7c379144ab
Matthew Bliffert <mattbliffert@icloud.com>
2018-09-14 17:17:10 -0400

    Activate analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,19 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography.js',
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-125832230-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
+      },
+    },
   ],
 };
